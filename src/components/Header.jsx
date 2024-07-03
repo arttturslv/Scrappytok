3d98445db090961ba97eb83b1496a67eaac4c294
arttturslv <arttturslv@gmail.com>
2024-07-02 21:53:26 -0300

    feat: added basic header

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="w-full top-0 absolute">
+            <h2 className="font-black text-4xl bg-inherit py-4 px-12">SCRAPPYTOK</h2>
+        </div>
+    )
+}
