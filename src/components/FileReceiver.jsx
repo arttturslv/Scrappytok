cce2e7edbd6854ef79f30b5dfb59cc5268e61ef0
arttturslv <arttturslv@gmail.com>
2024-07-03 08:12:23 -0300

    feat: basic component with input file

---
+++ src/components/FileReceiver.jsx
@@ -0,0 +1,48 @@
+
+export default function FileReceiver({file, setFile, setProgress}) {
+    async function FileUploader (fileTarget) {        
+        const fr = new FileReader();
+
+        fr.readAsText(fileTarget);
+
+        fr.addEventListener('load', () => {
+            setFile(JSON.parse(fr.result));
+            console.log(JSON.parse(fr.result).Activity["Favorite Videos"].FavoriteVideoList)
+            console.log(JSON.parse(fr.result))
+
+        })
+    }
+
+
+    return (
+        <div className='h-full space-y-8 md:flex md:justify-center'>
+        <h2 className='font-black text-5xl bg-inherit py-4 px-12'>Web tool to show data from your tiktok or instagram</h2>
+        <div className='md:mx-32 mx-8 flex flex-col items-center space-y-2'>
+            <div className='w-[100%] h-[100%] max-h-16 max-w-96 gap-4 border-yellow-500 flex justify-center items-center border-4 rounded-xl'>
+              <h3 className='font-black text-[3xl] '>drop a file </h3>
+              <img width={"8%"} src="../src/assets/arrow-icon.svg" alt="" srcSet="" />
+            </div>
+            <div className='w-[100%] h-[100%] max-h-72 max-w-96 border-yellow-500 flex justify-center items-center border-4 rounded-xl'>
+            <label htmlFor="avatar" className='cursor-pointer w-max flex-col flex items-center p-4'>
+              {
+                file==null?
+                    <img className="md:w-[100%] w-[80%] "  src="../src/assets/file-question-icon.svg" alt="" />
+                :
+                  <>
+                    <img className="w-[100%]" src="../src/assets/file-icon.svg" alt="" />
+                    <p>file added</p>
+                  </>
+
+              }
+            </label>
+            <input onChange={(e)=> FileUploader(e.target.files[0])} className='hidden' type="file" id="avatar" name="avatar" accept="*" />
+            </div>
+            <div onClick={()=> setProgress(1)} className='w-[100%] h-[100%] gap-4 max-h-72 max-w-96 border-yellow-500 flex justify-center items-center border-4 rounded-xl'>
+              <h3 className='font-black text-2xl '>Continuar</h3>
+              <img width={"8%"} className=" -rotate-90" src="../src/assets/arrow-icon.svg" alt="" srcSet="" />
+            </div>
+        </div>
+      </div>
+
+    )
+}
