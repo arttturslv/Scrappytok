c7581bfd5ba3d8fdf7d416b597c7f34ec48211c5
arttturslv <arttturslv@gmail.com>
2024-07-03 18:01:07 -0300

    feat: added filter (favorite & delete)

--- src/components/Element.jsx
+++ src/components/Element.jsx
@@ -1,21 +1,38 @@
-import {useState } from "react";
+import { useState } from "react";
 
-export default function Element({ link, date, focus, i }) {
+export default function Element({ link, date, focus, i, filterNet }) {
+    const [filter, setFilter] = useState('normal');
 
     function formataData(date) {
         return date.replaceAll('-', '/').substring(0, 10);
     }
-    
-        return (
-            <div style={focus==i?{border:'4px solid #000'}:{border:'none'}} className='w-32 h-48 bg-slate-600 relative flex flex-col justify-end group cursor-pointer'  >
-                <div className="w-[100%] h-[100%] group-hover:block">
-                 
-                    <div className="bg-slate-600 w-[100%] h-[100%]" onClick={()=> window.open(link, 'blank')}>
-                    </div>
+
+    return (
+        <div
+
+            style={
+                filter === filterNet
+                    ?
+                    focus == i
+                        ?
+                        { border: '4px solid #000' }
+                        :
+                        {}
+                    :
+                    { width: '0px', margin: '0px' }
+            }
+            className='w-32 h-48 bg-slate-50 m-[4px] transition-all duration-100 cursor-pointer flex'  >
+
+            <div className="bg-inherit flex flex-col w-full">
+                <div className="bg-inherit flex-grow-0 flex">
+                    <p className="bg-red-400" onClick={() => setFilter('del')}>🗑</p>
+                    <p className="w-full text-center bg-yellow-200" onClick={() => setFilter('fav')}>⭐</p>
                 </div>
-                <p className="text-white bg-[#fa51a2] w-full text-center">{formataData(date)}</p>    
+                <div className="bg-slate-600 flex-grow" onClick={() => window.open(link, 'blank')}></div>
+                <div className="bg-inherit  flex-grow-0"><p className="text-white bg-[#fa51a2] w-full text-center h-fit">{formataData(date)}</p></div>
             </div>
-        )
-    
+
+        </div>
+    )
 
 }
--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -6,6 +6,7 @@ import Element from "../components/Element";
 export default function View() {
     const [file, setFile] = useState('');
     const [focus, setFocus] = useState(null);
+    const [filterNet, setFilterNet] = useState('normal');
 
     const {id} = useParams();
 
@@ -24,16 +25,26 @@ export default function View() {
     }
 
     return (
-        <div className="flex mx-32 gap-8 md:flex-row items-center flex-wrap">
+        <div className="flex mx-32 gap-8 md:flex-row items-center justify-center flex-wrap">
             <Header/>
 
+            <div className="w-full h-12 flex justify-evenly">
+                <h1 className="font-bold text-xl bg-blue-400 h-fit px-8 py-2 cursor-pointer" onClick={() => setFilterNet('normal')}>👀 All</h1>
+                <h1 className="font-bold text-xl bg-yellow-400 h-fit px-8 py-2  cursor-pointer" onClick={() => setFilterNet('fav')}>⭐ Saved</h1>
+                <h1 className="font-bold text-xl bg-red-400 h-fit px-8 py-2  cursor-pointer" onClick={() => setFilterNet('del')}>🗑 Deleted</h1>
+                <h1 className="font-bold text-xl bg-green-600 h-fit px-4 py-2  cursor-pointer">💾</h1>
+            </div>
+
+        <div className="flex flex-wrap transition-all duration-500 w-fit justify-center">
             {
                 file.length>0 && file.map((item, i) => (
-                    <div key={i} onClick={()=> setFocus(i)}>
-                        <Element i={i} focus={focus} link={item.Link} date={item.Date}></Element>
+                    <div className="m-0 p-0" key={i} onClick={()=> setFocus(i)}>
+                        <Element i={i} filterNet={filterNet} focus={focus} link={item.Link} date={item.Date}></Element>
                     </div>
                 ))
             }
+        </div>
+     
         </div>
     )
 }
