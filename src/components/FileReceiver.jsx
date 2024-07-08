bab5070a9dd0ada0c70a1c3bf60071ac8d1b6512
arttturslv <arttturslv@gmail.com>
2024-07-08 09:12:04 -0300

    feat: added a file saver with localStorage
    obs: now users can upload a file, and this file will stay saved on browser storage

--- src/components/FileReceiver.jsx
+++ src/components/FileReceiver.jsx
@@ -1,20 +1,24 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 export default function FileReceiver({file, setFile}) {
-    
+    const [existingFile, setExistingFile] = useState(null);
+
     function FileUploader (fileTarget) {   
         if(!fileTarget) {
           console.error("File undefined")
           return;
         }
-      
+
+        console.log(fileTarget.name);
+
         const fr = new FileReader();
 
         fr.readAsText(fileTarget);
 
         fr.onload = () => {
           const parsedData = JSON.parse(fr.result);
-          setFile(parsedData);
-          console.log(parsedData.Activity["Favorite Videos"].FavoriteVideoList);
+          setFile(parsedData.Activity);
+          localStorage.setItem('ScrappyTok', JSON.stringify({data: parsedData.Activity, name: fileTarget.name}));
           console.log(parsedData);
         };
       
@@ -23,10 +27,21 @@ export default function FileReceiver({file, setFile}) {
         };
     }
 
+    useEffect(() => {
+      const localFile = localStorage.getItem('ScrappyTok');
+      if(localFile!=null) {
+        console.log(JSON.parse(localFile))
+        console.log('Já tem um arquivo salvo.')
+        setFile(JSON.parse(localFile));
+      } 
+    }, [])
 
     return (
         <div className='h-full space-y-8 md:flex md:justify-center'>
-        <h2 className='font-black text-5xl bg-inherit py-4 px-12'>Web tool to show data from your tiktok or instagram</h2>
+          <div>
+            <h2 className='font-black text-5xl py-4 px-12'>Web tool to show data from your tiktok JSON file</h2>
+          </div>
+
         <div className='md:mx-32 mx-8 flex flex-col items-center space-y-2'>
             <div className='w-[100%] h-[100%] max-h-16 max-w-96 gap-4 border-yellow-500 flex justify-center items-center border-4 rounded-xl'>
               <h3 className='font-black text-[3xl] '>drop a file </h3>
@@ -40,14 +55,14 @@ export default function FileReceiver({file, setFile}) {
                 :
                   <>
                     <img className="w-[100%]" src="../src/assets/file-icon.svg" alt="" />
-                    <p>file added</p>
+                    <p className="font-medium">{file.name}</p>
                   </>
 
               }
             </label>
             <input onChange={(e)=> FileUploader(e.target.files[0])} className='hidden' type="file" id="avatar" name="avatar" accept="*" />
             </div>
-            <Link to={'/options'} className='w-[100%] h-[100%] gap-4 max-h-72 max-w-96 border-yellow-500 flex justify-center items-center border-4 rounded-xl'>
+            <Link to={'/options'} className='w-[100%] h-[100%] gap-4 max-h-16 max-w-96 border-yellow-500 flex justify-center items-center border-4 rounded-xl'>
               <h3 className='font-black text-2xl '>Continuar</h3>
               <img width={"8%"} className=" -rotate-90" src="../src/assets/arrow-icon.svg" alt="" srcSet="" />
             </Link>
